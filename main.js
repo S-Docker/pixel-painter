495edd5ea4a948bfb493cbdd11e3dd66efacdccd
S-Docker <steven.docker@hotmail.co.uk>
2022-01-17 18:34:01 +0000

    event listeners created to provide basic drawing functionality

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 const gridContainer = document.querySelector("#grid-container");
+let gridPixels;
+let drawState = false;
+
+/*
+ * Keeps track of whether left click is being held down, allows
+ * user to keep drawing without clicking individual pixels
+*/
+function initialiseMouseClickDetection(){
+    window.addEventListener('mousedown', (e) => {
+        if (e.button === 0){
+            drawState = true;
+        }
+    })
+
+    window.addEventListener('mouseup', (e) => {
+        if (e.button === 0){
+            drawState = false;
+        }
+    });
+}
 
 function generateGrid(size){
     // Set grid containers maximum items per column equal to size parameter
@@ -8,7 +28,33 @@ function generateGrid(size){
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
         gridContainer.appendChild(pixel); 
+        
+        AddMouseoverAndMousedownEvent(pixel);
+        disableDragDropFunctionality(pixel);
     }  
 }
 
-document.onload = generateGrid(16);
+function AddMouseoverAndMousedownEvent(pixel){
+    pixel.addEventListener('mousedown', colorPixel);
+
+    pixel.addEventListener('mouseover', (e) => {
+        if (drawState){
+            colorPixel(e);
+        }
+    });
+}
+
+function disableDragDropFunctionality(pixel){
+    ['dragstart', 'drop'].forEach(e => {
+        pixel.addEventListener(e, (e) => {
+            e.preventDefault();
+        });
+    });
+}
+
+function colorPixel(pixel){
+    pixel.currentTarget.style.backgroundColor = "black";
+}
+
+generateGrid(16);
+initialiseMouseClickDetection();
